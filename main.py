c2b7a00e30da4c97e803e5a51e50f0ad1bb6a5f0
lenya1567 <55053376+lenya1567@users.noreply.github.com>
2020-07-26 17:36:09 +0300

    Update main.py

--- main.py
+++ main.py
@@ -78,24 +78,3 @@ def run(*objs):
 
     return finalCode
 
-class PassParams:
-    def __init__(self, varsInit, pageView, btnClick1, btnClick2, btnClick3, keyboard, globalInclude):
-        self.value = {
-            "vars": varsInit,
-            "view": pageView,
-            "Btn1": btnClick1,
-            "Btn2": btnClick2,
-            "Btn3": btnClick3,
-            "KeyB": keyboard,
-            "global": globalInclude
-        }
-    def current(self):
-        return self.value
-
-class Current:
-    def __init__(self):
-        self.PassParams = PassParams
-        self.Page = Page
-        self.run = run
-
-export = Current()
